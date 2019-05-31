a7e4260557f3709ead0eebd601f99a5b7a0489f4
Kolya Bezgin <bezginn@edi-n.com>
2019-05-31 15:22:17 +0300

    wefwef

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,12 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+sys.path.insert(0, os.path.abspath('sphinxcontrib'))
+
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxcontrib.contentui',
+    'contentui',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
